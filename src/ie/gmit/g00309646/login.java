b85da46e56206e270c0c8365ef59131e3312e547
Adrian Sypos <adrian00935@gmail.com>
2017-04-20 14:35:14 +0100

    Added comments for login frame

--- src/ie/gmit/g00309646/login.java
+++ src/ie/gmit/g00309646/login.java
@@ -22,6 +22,7 @@ import java.awt.Font;
 
 public class login extends JFrame {
 
+	// Declaring all variables for this class
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField inputUsername;
@@ -40,6 +41,7 @@ public class login extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				
+				// BeautyEye API to make the application look better
 				try {
 					org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
 					UIManager.put("RootPane.setupButtonVisible", false);
@@ -48,14 +50,20 @@ public class login extends JFrame {
 					e1.printStackTrace();
 				}
 				
+				// Create the frame
 				try {
 					login frame = new login();
+					// Make the frame appear in the middle of the screen
 				    frame.setLocationRelativeTo(null);
+				    // Set the frame visible
 					frame.setVisible(true);
 					
+					// Try to connect to database
 					try (Connection conn = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+dbPort+"/"+dbNameCon+"?useSSL=false", dbUsername, dbPassword); Statement stmt = conn.createStatement();){
+						// Enable the login button only if access to database has been successfull
 						btnNewButton.setEnabled(true);
 						
+						// Close connection
 						stmt.close();
 						conn.close();
 						
@@ -67,6 +75,7 @@ public class login extends JFrame {
 					
 				} catch (Exception e) {
 					e.printStackTrace();
+					// Popup message to inform user that the connection cannot be aquired 
 					JOptionPane.showMessageDialog(null, e);
 				}
 			}
@@ -77,6 +86,7 @@ public class login extends JFrame {
 	 * Create the frame.
 	 */
 	public login() {
+		// Set the default font for this frame
 		setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		setResizable(false);
 		setTitle("IrishBusApp - Login");
@@ -88,25 +98,31 @@ public class login extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		// New button and listener
 		btnNewButton = new JButton("Login");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				// Variables that will hold information that will later be user to collect from database
 				String guiUser, guiPass, dbUser = null, dbPass = null, dbName = null;
 				char[] pass;
 				boolean is_admin;
 				int userID;
 				
+				// Variables from the fields to compare with the ones from database
 				guiUser = inputUsername.getText();;
 				pass = passwordField.getPassword();
 				guiPass = String.valueOf(pass);;
 				
 				//System.out.println(guiUser+" "+guiPass);
 				
+				// Connect to database
 				try (Connection conn = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+dbPort+"/"+dbNameCon+"?useSSL=false", dbUsername, dbPassword); Statement stmt = conn.createStatement();){
 
+					 // Query String
 					 String strSelect = "select id, name, username, password, is_admin from users";
 			 
+					 // Execute the statement
 			         ResultSet rset = stmt.executeQuery(strSelect);
 			 
 			         // Step 4: Process the ResultSet by scrolling the cursor forward via next().
@@ -120,18 +136,26 @@ public class login extends JFrame {
 			            //System.out.println(dbUser);
 			            //System.out.println(dbPass);
 			            
+			            // Compare the username and password from the text fields to the username and password from the database
 			            if(guiUser.equalsIgnoreCase(dbUser) && guiPass.equalsIgnoreCase(dbPass)){
+			            	// if user has been successfully found in the database, check if the user is admin
 			            	if(is_admin){
+			            		// Welcome popup message to inform user that he has admin privileges
 			            		JOptionPane.showMessageDialog(null, "Hi, " + dbName + "." + "You have admin privileges.");
+			            		// Hide the login frame
 			            		hideLogin();
+			            		// Create and show the main window frame in the middle of the screen
 				            	mainWindow f = new mainWindow(true, userID);
 				            	f.setLocationRelativeTo(null);
 				            	f.setVisible(true);
 				            	break;
 			            	}
 			            	else{
+			            		// Welcome popup message displaying users Name
 			            		JOptionPane.showMessageDialog(null, "Hi, " + dbName + ".");
+			            		// Hide the login frame
 			            		hideLogin();
+			            		// Create and show the main window frame in the middle of the screen
 				            	mainWindow f = new mainWindow(false, userID);
 				            	f.setLocationRelativeTo(null);
 				            	f.setVisible(true);
@@ -141,11 +165,13 @@ public class login extends JFrame {
 			            	
 			            }
 			         }
-			         
+			         // If the username and/or password has not been found in the database
 			         if(!(guiUser.equalsIgnoreCase(dbUser) && guiPass.equalsIgnoreCase(dbPass))){
+			        	 // popup message for user to inform that he entered wrong details
 			        	 JOptionPane.showMessageDialog(null, "Wrong details, try again..");
 			         }
 			         
+			         // close the connection
 			        rset.close();
 			        stmt.close();
 					conn.close();
@@ -159,9 +185,11 @@ public class login extends JFrame {
 		btnNewButton.setBounds(10, 70, 100, 24);
 		contentPane.add(btnNewButton);
 		
+		// Exit button and its listener
 		JButton btnNewButton_1 = new JButton("Exit");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				// popup message to thank user for using this software
 				JOptionPane.showMessageDialog(null, "Thank you for using this software.");
 				System.exit(0);
 			}
@@ -191,6 +219,7 @@ public class login extends JFrame {
 		contentPane.add(passwordField);
 	}
 	
+	// function to dispose this frame
 	public void hideLogin(){
 		this.dispose();
 	}
